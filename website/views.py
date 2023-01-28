82a2e46154783a5911b0d9627df0130c7aaea888
elafosse <elafosse@outlook.com>
2023-01-28 03:53:14 -0500

    Saved Markers

--- website/views.py
+++ website/views.py
@@ -1,8 +1,17 @@
 """ Service related functions """
-from flask import Blueprint, render_template
-import requests
+from flask import Blueprint, render_template, request
+# from flask_cors import CORS
+# import json
 
 views = Blueprint("views", __name__)
+# CORS(views)
+
+
+def saveMarkers(markers):
+    with open('markers.txt', 'w') as f:
+        for m in markers:
+            f.write(f'{m}\n')    
+
 
 @views.route("/")
 @views.route("home")
@@ -10,6 +19,8 @@ def home():
     return render_template("home.html")
 
 
-@views.route("/map")
+@views.route("/map", methods=['GET', 'POST'])
 def map():
+    if request.method == 'POST':
+        saveMarkers(request.json['values'])
     return render_template('map.html')
